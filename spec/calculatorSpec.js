d85f76687a7d495d42f17f38205e1bb56d00de54
Sherfin S <sherfin94@gmail.com>
2016-07-26 13:40:43 +0530

    put tests for each function inside corresponding describe blocks

--- spec/calculatorSpec.js
+++ spec/calculatorSpec.js
@@ -1,22 +1,30 @@
 
-describe("Calculator", function() {
-  it(" returns 5 when 3 is added to 2", function() {
-    expect(calculator['+'](3, 2)).toEqual(5);
+  describe("Calculator", function() {
+    describe("addition", function() {
+    it(" returns 5 when 3 is added to 2", function() {
+      expect(calculator['+'](3, 2)).toEqual(5);
+    });
+
+
+
+    it(" returns -1 when -3 is added to 2", function() {
+      expect(calculator['+'](-3, 2)).toEqual(-1);
+    });
   });
 
-  it(" returns -1 when -3 is added to 2", function() {
-    expect(calculator['+'](-3, 2)).toEqual(-1);
-  });
+    describe("subtraction", function() {
+    it("returns 5 when 10 is subtracted from 15", function() {
+      expect(calculator['-'](15, 10)).toEqual(5);
+    });});
 
-  it("returns 5 when 10 is subtracted from 15", function() {
-    expect(calculator['-'](15, 10)).toEqual(5);
-  });
+    describe("multiplication", function() {
+    it("returns 10 when 5 is multiplied with 2", function() {
+      expect(calculator['*'](5, 2)).toEqual(10);
+    });
+    });
 
-  it("returns 10 when 5 is multiplied with 2", function() {
-    expect(calculator['*'](5, 2)).toEqual(10);
+    describe("division", function() {
+    it("returns 5 when 10 is divided by 2", function() {
+      expect(calculator['/'](10, 2)).toEqual(5);
+    });});
   });
-
-  it("returns 5 when 10 is divided by 2", function() {
-    expect(calculator['/'](10, 2)).toEqual(5);
-  });
-});

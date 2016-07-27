2dbafd6b499be41db08daabc84ed38eee48372a1
Sherfin S <sherfin94@gmail.com>
2016-07-27 10:57:03 +0530

    Move parseFloat outside the calculator object

--- src/calculator.js
+++ src/calculator.js
@@ -1,29 +1,17 @@
 var calculator = {
   '+' : function add(a ,b) {
-    a = parseFloat(a);
-    b = parseFloat(b);
-
     return a + b;
   },
 
   '-' : function(a, b) {
-    a = parseFloat(a);
-    b = parseFloat(b);
-
     return a - b;
   },
 
   '*' : function(a, b) {
-    a = parseFloat(a);
-    b = parseFloat(b);
-
     return a * b;
   },
 
   '/' : function(a, b) {
-    a = parseFloat(a);
-    b = parseFloat(b);
-
     return a / b;
   }
 }
